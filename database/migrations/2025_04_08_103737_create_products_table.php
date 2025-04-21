bc8cdc5bf7e56bfca3bc25f637c1680a1bac3d50
nemigazanlola <csordas.lola@blathy.info>
2025-04-21 13:56:08 +0200

    lefut a seed

--- database/migrations/2025_04_08_103737_create_products_table.php
+++ database/migrations/2025_04_08_103737_create_products_table.php
@@ -14,13 +14,13 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->string("price");
+            $table->integer("price");
             $table->string("image_link");
             $table->string("description");
             $table->integer("rating");
             $table->integer("sold_quantity")->default(0);
-            $table->foreignId('brand_id')->constrained('brands');
-            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('brand_id')->constrained();
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
             $table->softDeletes();
         });
